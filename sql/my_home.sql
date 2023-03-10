de83e6d9a62f84c34bc43e856baf10f72ffdcfea
Victor Vargas <victorvdvg03@gmail.com>
2023-03-09 21:42:40 -0800

    Changed Laravel Project to raw PHP

---
+++ sql/my_home.sql
@@ -0,0 +1,121 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 08, 2023 at 03:47 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `my_home`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone_num` int(11) NOT NULL,
+  `family_income` float NOT NULL,
+  `num_cars` int(11) NOT NULL,
+  `misc_expenses` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `home`
+--
+
+CREATE TABLE `home` (
+  `address` varchar(255) NOT NULL,
+  `lot_size` float NOT NULL,
+  `cooling_type` varchar(128) NOT NULL,
+  `construction_type` varchar(128) NOT NULL,
+  `garage_size` float NOT NULL,
+  `year_built` int(11) NOT NULL,
+  `property_type` varchar(128) NOT NULL,
+  `heating_type` varchar(128) NOT NULL,
+  `heating_time` time NOT NULL,
+  `num_floors` int(11) NOT NULL,
+  `floor_space` float NOT NULL,
+  `roof` float NOT NULL,
+  `bathrooms` int(11) NOT NULL,
+  `foundation` varchar(128) NOT NULL,
+  `bedrooms` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `provider`
+--
+
+CREATE TABLE `provider` (
+  `name` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service`
+--
+
+CREATE TABLE `service` (
+  `name` varchar(255) NOT NULL,
+  `type` varchar(128) NOT NULL,
+  `provider` varchar(255) NOT NULL,
+  `cost` float NOT NULL,
+  `terms` varchar(2048) NOT NULL,
+  `penalty` varchar(1028) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`name`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `phone_num` (`phone_num`);
+
+--
+-- Indexes for table `home`
+--
+ALTER TABLE `home`
+  ADD PRIMARY KEY (`address`);
+
+--
+-- Indexes for table `provider`
+--
+ALTER TABLE `provider`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Indexes for table `service`
+--
+ALTER TABLE `service`
+  ADD PRIMARY KEY (`name`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
