8f62585a1a3129238cfd3e98a02f1f984dfc5d35
Victor Vargas <victorvdvg03@gmail.com>
2023-03-09 17:20:23 -0800

    Added the Website Design

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/signup', function () {
+    return view('signup');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/home', function () {
+    return view('home');
 });
